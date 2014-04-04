35eb502b8b2fe4240f96adeb60cb41683032fcb6
vilbeyli <volkanilbeyli@gmail.com>
2014-04-04 22:28:49 +0300

    "After hit" laser beam issue fixed.

--- Assets/Scripts/PlayerScripts/LaserScript.cs
+++ Assets/Scripts/PlayerScripts/LaserScript.cs
@@ -11,6 +11,8 @@ public class LaserScript : MonoBehaviour
 	private Vector3 endPosition;
 	private RaycastHit hit;
 	private PlayerController player;
+
+	private bool justHit = false;
 	
 	// Use this for initialization
 	void Start () 
@@ -39,43 +41,15 @@ public class LaserScript : MonoBehaviour
 			// Collision
 			if(Physics.Raycast(transform.position, new Vector3(0,1,0), out hit, 12.58f, 1<<8))
 			{
-				//print(hit.collider.gameObject.tag + " " + hit.collider.gameObject.name);
-				switch(hit.collider.gameObject.tag)
-				{
-				case "Enemy":
-					// adjust laser beam position
-					endPosition = new Vector3(startPosition.x,
-					                          hit.collider.gameObject.transform.position.y, 0f);
-					
-					laser.SetPosition(0, startPosition);
-					laser.SetPosition(1, endPosition);
-
-					// trigger laser hit events
-					switch(hit.collider.gameObject.name)
-					{
-					case "L1Enemy(Clone)":
-					case "L1Enemy":
-						hit.collider.gameObject.GetComponent<L1EnemyScript>().hp -= 5;
-						break;
-					case "L2Enemy(Clone)":
-					case "L2Enemy":
-						hit.collider.gameObject.GetComponent<L2EnemyScript>().hp -= 5;
-						break;
-					case "L3Enemy(Clone)":
-					case "L3Enemy":
-						hit.collider.gameObject.GetComponent<L3EnemyScript>().hp -= 5;
-						break;
-					}
-					
-					break;
-
-				}
+				justHit = true;
+				processHit (hit.collider.gameObject.tag);
 			}
-			else {
-				startPosition = GameObject.FindGameObjectWithTag("Player").transform.position;
+			else 
+			{
 				endPosition = new Vector3(startPosition.x, 9.9f, 0);
-				laser.SetPosition(0, startPosition);
-				laser.SetPosition(1, endPosition);
+				laser.SetPosition(0, startPosition);	
+				laser.SetPosition(1, endPosition);		
+				justHit = false;
 			}
 
 			// play the laser effect
@@ -86,29 +60,72 @@ public class LaserScript : MonoBehaviour
 		// Fading effect
 		if(justFired == true)
 		{
-			// update laser beam position
+			// update laser beam position (prevent bending after shooting)
 			laser.SetPosition(0, transform.position);
-			if(Physics.Raycast(transform.position, new Vector3(0,1,0), out hit, 12.58f, 1<<8))
+			if(Physics.Raycast(transform.position, new Vector3(0,1,0), out hit, 12.58f, 1<<8)){
 				endPosition = new Vector3(transform.position.x, endPosition.y, 0f);
-			else
+				if(justHit == false && timeElapsed <= 0.1f)
+				{	// if laser didin't initially hit but hit after updating position (while moving)
+					processHit(hit.collider.gameObject.tag);
+					justHit = true;
+				}
+			}
+			else  // if still no hit, let laser go to the edge of the screen
 				endPosition = new Vector3(transform.position.x, 9.9f, 0);
 			laser.SetPosition(1, endPosition);
 
+			// apply fading effect to the laser beam
 			timeElapsed += Time.deltaTime;
-			//print ("Time elapsed: " + timeElapsed);
 			if(timeElapsed > 0.05f) laser.SetWidth(0.05f, 0.05f);
 			if(timeElapsed > 0.075f) laser.SetWidth(0.025f, 0.025f);
 			if(timeElapsed > 0.1f)
-			{
+			{	// laser fades completely after 0.1 seconds.
 				laser.SetPosition(0, new Vector3(0,0,0));
 				laser.SetPosition(1, new Vector3(0,0,0));
 				laser.SetWidth(0.1f, 0.1f);
 			}
-			if(timeElapsed > 0.26)
+
+			// apply laser cannon cooldown
+			if(timeElapsed > 0.26)	
 			{
 				timeElapsed = 0.0f;
 				justFired = false;
 			}
 		}
 	}
+
+	void processHit(string tag)
+	{
+		switch(tag)
+		{
+		case "Enemy":
+			// adjust laser beam position
+			endPosition = new Vector3(startPosition.x,
+			                          hit.collider.gameObject.transform.position.y, 0f);
+			
+			laser.SetPosition(0, startPosition);
+			laser.SetPosition(1, endPosition);
+			
+			// trigger laser hit events
+			switch(hit.collider.gameObject.name)	
+			{
+			case "L1Enemy(Clone)":
+			case "L1Enemy":
+				hit.collider.gameObject.GetComponent<L1EnemyScript>().hp -= 5;
+				break;
+			case "L2Enemy(Clone)":
+			case "L2Enemy":
+				hit.collider.gameObject.GetComponent<L2EnemyScript>().hp -= 5;
+				break;
+			case "L3Enemy(Clone)":
+			case "L3Enemy":
+				hit.collider.gameObject.GetComponent<L3EnemyScript>().hp -= 5;
+				break;
+			}
+			
+			break;
+			
+		}
+	}
+
 }
